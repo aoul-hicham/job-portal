6c3f4dcfd79f949414c9ad9967715707058a6e98
Hicham elaoula <aoul.hicham@gmail.com>
2024-04-28 12:17:37 +0100

    handling authentication based on user role

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -10,33 +10,33 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\View\View;
 
-class AuthenticatedSessionController extends Controller
-{
+class AuthenticatedSessionController extends Controller {
     /**
      * Display the login view.
      */
-    public function create(): View
-    {
+    public function create(): View {
         return view('auth.login');
     }
 
     /**
      * Handle an incoming authentication request.
      */
-    public function store(LoginRequest $request): RedirectResponse
-    {
+    public function store(LoginRequest $request): RedirectResponse {
         $request->authenticate();
 
         $request->session()->regenerate();
 
-        return redirect()->intended(RouteServiceProvider::HOME);
+        return ($request->user()->role == 'company') ? redirect()->intended(RouteServiceProvider::COMPANY_HOME) : (($request->user()->role == 'candidate') ? redirect()->intended(RouteServiceProvider::CANDIDATE_HOME) :
+            $this->destroy($request)
+        );
+
+        // return redirect()->intended(RouteServiceProvider::HOME);
     }
 
     /**
      * Destroy an authenticated session.
      */
-    public function destroy(Request $request): RedirectResponse
-    {
+    public function destroy(Request $request): RedirectResponse {
         Auth::guard('web')->logout();
 
         $request->session()->invalidate();
--- routes/web.php
+++ routes/web.php
@@ -22,10 +22,21 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+// Company role based route
+Route::get('company/dashboard', function () {
+    return view('frontend.company-dashboard.dashboard');
+})->middleware(['auth', 'verified'])->name('company.dashboard');
+
+
+// Candidate role based route
+Route::get('candidate/dashboard', function () {
+    return view('frontend.candidate-dashboard.dashboard');
+})->middleware(['auth', 'verified'])->name('candidate.dashboard');
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
